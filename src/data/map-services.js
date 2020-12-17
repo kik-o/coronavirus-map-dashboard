7774dc9e84e3bcf3ca74ce97ecda3c85994d916c
Kiko Estrada <uldarico@outlook.com>
2020-12-17 13:32:25 -0800

    🔌 Update Mapbox API key

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -10,6 +10,6 @@ export const mapServices = [
     name: 'Mapbox',
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    url: `https://api.mapbox.com/styles/v1/colbyfayock/ck8c2foj72lqk1jnug0g2haw0/tiles/256/{z}/{x}/{y}@2x?access_token=${accessToken}`,
+    url: `mapbox://styles/kikoestrada/ckis99wn60q9319p6nc9qv5sx`,
   },
 ];
