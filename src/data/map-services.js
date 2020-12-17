6449a8d61bf562e8939cbd27afe1670cd13f2536
Kiko Estrada <uldarico@outlook.com>
2020-12-17 14:04:19 -0800

    🎨 Fix URL Scheme for Mapbox Styling API

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -1,5 +1,3 @@
-const accessToken = process.env.GATSBY_MAPBOX_KEY;
-
 export const mapServices = [
   {
     name: 'OpenStreetMap',
@@ -10,6 +8,7 @@ export const mapServices = [
     name: 'Mapbox',
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    url: `mapbox://styles/kikoestrada/ckis99wn60q9319p6nc9qv5sx`,
+    url:
+      'https://api.mapbox.com/styles/v1/kikoestrada/ckis99wn60q9319p6nc9qv5sx.html?fresh=true&title=view&access_token=pk.eyJ1Ijoia2lrb2VzdHJhZGEiLCJhIjoiY2tpczhtZmNkMGZjazJ1cngxZDJubHJsdyJ9.LKpfd80MZsE_Nh0enBw2tA',
   },
 ];
